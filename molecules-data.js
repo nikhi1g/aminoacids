f656b9c0a4a800984eed217bc7dd819183614a21
nikhi1g <2nikhilg@gmail.com>
2026-01-18 16:37:52 -0800

    feat: complete implementation of molecules page and fix chapter1 logic

---
+++ molecules-data.js
@@ -0,0 +1,6 @@
+const moleculesData = [
+    { name: "Water", abbr: "H2O", smiles: "O", tags: ["solvent", "polar"] },
+    { name: "Phosphate", abbr: "Pi", smiles: "O=P(O)(O)O", tags: ["ion", "acidic"] },
+    { name: "Lysine", abbr: "Lys", smiles: "NCCCCC(N)C(=O)O", tags: ["amino-acid", "basic"] },
+    { name: "Arginine", abbr: "Arg", smiles: "NC(N)=NCCCC(N)C(=O)O", tags: ["amino-acid", "basic"] }
+];
