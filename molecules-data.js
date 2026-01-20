3303d69d449fc615bf17f5366fb9266136b30856
nikhi1g <2nikhilg@gmail.com>
2026-01-20 15:44:38 -0800

    feat: add 5 nitrogenous bases to molecules data

--- molecules-data.js
+++ molecules-data.js
@@ -2,5 +2,10 @@ const moleculesData = [
     { name: "Water", abbr: "H2O", smiles: "O", tags: ["solvent", "polar"] },
     { name: "Phosphate", abbr: "Pi", smiles: "O=P(O)(O)O", tags: ["ion", "acidic"] },
     { name: "Lysine", abbr: "Lys", smiles: "NCCCCC(N)C(=O)O", tags: ["amino-acid", "basic"] },
-    { name: "Arginine", abbr: "Arg", smiles: "NC(N)=NCCCC(N)C(=O)O", tags: ["amino-acid", "basic"] }
+    { name: "Arginine", abbr: "Arg", smiles: "NC(N)=NCCCC(N)C(=O)O", tags: ["amino-acid", "basic"] },
+    { name: "Adenine", abbr: "Ade", smiles: "Nc1ncnc2[nH]cnc12", tags: ["purine", "base"] },
+    { name: "Guanine", abbr: "Gua", smiles: "Nc1nc2[nH]cnc2c(=O)[nH]1", tags: ["purine", "base"] },
+    { name: "Cytosine", abbr: "Cyt", smiles: "Nc1cc(=O)[nH]cn1", tags: ["pyrimidine", "base"] },
+    { name: "Thymine", abbr: "Thy", smiles: "Cc1c[nH]c(=O)[nH]c1=O", tags: ["pyrimidine", "base"] },
+    { name: "Uracil", abbr: "Ura", smiles: "O=c1cc[nH]c(=O)[nH]1", tags: ["pyrimidine", "base"] }
 ];
